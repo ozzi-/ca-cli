9aaf953bd211119ae116489c28122f2e7e92b7fb
ozzi- <oz@oz-web.com>
2018-04-12 13:36:06 +0200

    Create getcert.sh

---
+++ getcert.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ********
+# * INIT *
+# ********
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+if [ ! -f "rootpath" ]; then
+  echo "Run setup first"
+  exit 1
+fi
+rootpath=`cat rootpath`
+cd $rootpath
+
+
+# ************
+# * GET CERT *
+# ************
+
+echo ""
+printf "${YELLOW}Enter domain name of the certificate${NC}\n"
+read domain
+echo ""
+
+cat intermediate/certs/$domain.cert.pem
+cat intermediate/private/$domain.key.pem
